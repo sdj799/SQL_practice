2799e9c43186267ee1b570e937b47974e24cce18
dongjun <sdj799@naver.com>
2023-04-13 15:19:05 +0900

    04.13. commit

--- 05_숫자날짜함수.sql
+++ 05_숫자날짜함수.sql
@@ -45,9 +45,9 @@ FROM employees; -- 년수
 SELECT ROUND(sysdate) FROM dual;
 SELECT ROUND(sysdate, 'year') FROM dual; -- 년 기준으로 반올림
 SELECT ROUND(sysdate, 'month') FROM dual; -- 월 기준으로 반올림
-SELECT ROUND(sysdate, 'day') FROM dual; -- 일 기준으로 반올림 (해당 주의 일요일 날짜)
+SELECT ROUND(sysdate, 'day') FROM dual; -- 주 기준으로 반올림 (해당 주의 일요일 날짜)
 
 SELECT TRUNC(sysdate) FROM dual;
 SELECT TRUNC(sysdate, 'year') FROM dual; -- 년 기준으로 절사
 SELECT TRUNC(sysdate, 'month') FROM dual; -- 월 기준으로 절사
-SELECT TRUNC(sysdate, 'day') FROM dual; -- 일 기준으로 절사 (해당 주의 일요일 날짜)
+SELECT TRUNC(sysdate, 'day') FROM dual; -- 주 기준으로 절사 (해당 주의 일요일 날짜)
